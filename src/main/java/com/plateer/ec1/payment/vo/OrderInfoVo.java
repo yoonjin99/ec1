25a8560aa3522b767f3255cfbae79442d5f65436
yoonjin99 <dbswls1999@naver.com>
2022-07-19 15:40:06 +0900

    수정중

--- src/main/java/com/plateer/ec1/payment/vo/OrderInfoVo.java
+++ src/main/java/com/plateer/ec1/payment/vo/OrderInfoVo.java
@@ -9,11 +9,8 @@ import javax.validation.constraints.NotNull;
 public class OrderInfoVo {
     @NotNull(message = "주문번호가 null 입니다.")
     private String ordNo;
-    @NotNull
     private String goodName;
-    @NotNull
     private String buyerName;
-    @NotNull
     @Email(message = "유효하지 않은 이메일 형식입니다.")
     private String buyerEmail;
 }
